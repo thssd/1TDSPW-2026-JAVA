3fa60c578bf574d311c9c0331221c2b3085a6502
Thiago Shimizu Sodré dos Santos <thiagoshimizusodre@gmail.com>
2026-03-22 20:58:49 -0300

    Add files via upload

---
+++ 1TDSPW-2026-Apostila04/src/br/com/fiap/loja/model/Fornecedor.java
@@ -0,0 +1,7 @@
+package br.com.fiap.loja.model;
+
+public class Fornecedor {
+
+    public String nomeDoFornecedor;
+    public String cnpj;
+}
---
+++ 1TDSPW-2026-Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -0,0 +1,52 @@
+package br.com.fiap.loja.view;
+
+import br.com.fiap.loja.model.Fornecedor;
+import br.com.fiap.loja.model.Produto;
+
+import java.util.Scanner;
+
+//Responsável por iteragir com o usuário
+public class Terminal {
+    static void main() {
+        Scanner leitor = new Scanner (System.in);
+        //Ler as informaçãoes do produto (sem fornecedor por enquanto)
+        System.out.println("Digite o nome do produto");
+        String nome = leitor.nextLine();
+
+        System.out.println("Digite o preço do produto");
+        double preco = leitor.nextDouble();
+
+        System.out.println("Digite o código do produto");
+        int codigo = leitor.nextInt();
+
+        System.out.println("O produto está disponivel (true/false");
+        boolean disponivel = leitor.nextBoolean();
+
+        System.out.println("FORNECEDOR");
+        System.out.println("Digite o nome do fornecedor");
+        String nomeDoFornecedor = leitor.next();
+
+        System.out.println("Digite o CNPJ do fornecedor");
+        String cnpj = leitor.next();
+
+        //Instanciar a classe produto
+        Produto produto = new Produto();
+        Fornecedor fornecedor = new Fornecedor();
+
+        //Colocar as informações no objeto produto
+        produto.nome = nome;
+        produto.preco = preco;
+        produto.codigo = codigo;
+        produto.disponivel = disponivel;
+
+        //Exibir as informações do objeto produto
+        System.out.println("Nome do produto: " + nome);
+        System.out.println("Preço do produto: " + preco);
+        System.out.println("Codigo do produto: " + codigo);
+        System.out.println("Produto está disponível: " + disponivel);
+        System.out.println("FORNECEDOR");
+        System.out.println("Nome: " + nomeDoFornecedor);
+        System.out.println("CNPJ " + cnpj);
+
+    }
+}
