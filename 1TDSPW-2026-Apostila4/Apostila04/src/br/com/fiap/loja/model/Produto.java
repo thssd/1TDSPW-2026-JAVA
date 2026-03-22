22707726a2e1bd9512cbfb6b4df0b7a8f7de3f64
Thiago Shimizu Sodré dos Santos <thiagoshimizusodre@gmail.com>
2026-03-22 20:55:51 -0300

    Add files via upload

---
+++ 1TDSPW-2026-Apostila4/Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -0,0 +1,11 @@
+package br.com.fiap.loja.model;
+
+public class Produto {
+
+    public String nome;
+    public double preco;
+    public int codigo;
+    public boolean disponivel;
+    Fornecedor fornecedor;
+
+}
