49f2519e544fee55e89feb668e49176078b36036
Thiago Shimizu Sodré dos Santos <thiagoshimizusodre@gmail.com>
2026-03-22 20:56:46 -0300

    Add files via upload

---
+++ 1TDSPW-2026-Apostila3/Apostila03/src/ExemploOperadores.java
@@ -0,0 +1,25 @@
+public class ExemploOperadores {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+
+        n++; //Incrementa a variavel em 1 unidade
+        System.out.println(n);
+        n--; //Decrementa a variavel em 1 unidade
+        System.out.println(n);
+
+        //Somar 10 no n
+        n += 10; //n = n + 10;
+        System.out.println(n); // ?
+        n -= 50;
+        System.out.println(n); // ?
+        n *=3;
+        System.out.println(n); // ?
+        n /= 2;
+        System.out.println(n); // ?
+        n %= 7;
+        System.out.println(n); // ?
+
+    }
+}
---
+++ 1TDSPW-2026-Apostila3/Apostila03/src/Teste.java
@@ -0,0 +1,20 @@
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Boa noite!");
+
+        //Declarar variáveis
+        boolean ativo;
+        //Atribuir um valor para a variavel
+        ativo = true; //false
+        //Declarar variaveis e atribuir o valor
+        char sexo = 'F';
+        byte idade = 19;
+        short rm = 456;
+        int ano = 2026;
+        long id = 123;
+        float altura = 1.80f; //f no final para dizer que é um float
+        double preco = 150.25;
+        String nome = "Ricardo";
+    }
+}
