512cafd0baee1ae81776d1ccfd56d1cb76205829
thssd <thiagoshimizusodre@gmail.com>
2026-04-08 16:51:43 -0300

    feat: aula 04 exercicios

--- Apostila04-Exercicios/src/view/Terminal.java
+++ Apostila04-Exercicios/src/view/Terminal.java
@@ -7,31 +7,31 @@ import java.util.Scanner;
 public class Terminal {
     static void main() {
         Scanner leitor = new Scanner(System.in);
-        Aluno aluno = new Aluno();
+        Aluno aluno1 = new Aluno();
 
-        //Ler os dados do aluno
+        //Ler os dados do aluno1
         System.out.println("Digite seu nome");
-        aluno.nome = leitor.nextLine();
+        aluno1.nome = leitor.nextLine();
 
         System.out.println("Digite a nota da cp1");
-        aluno.cp1 = leitor.nextDouble();
+        aluno1.cp1 = leitor.nextDouble();
 
         System.out.println("Digite a nota da cp2");
-        aluno.cp2 = leitor.nextDouble();
+        aluno1.cp2 = leitor.nextDouble();
 
         System.out.println("Digite a nota do primeiro Challenge");
-        aluno.sprint1 = leitor.nextDouble();
+        aluno1.sprint1 = leitor.nextDouble();
 
         System.out.println("Digite a nota do segundo Challenge");
-        aluno.sprint2 = leitor.nextDouble();
+        aluno1.sprint2 = leitor.nextDouble();
 
         System.out.println("Digite da Global Solution");
-        aluno.gs = leitor.nextDouble();
+        aluno1.gs = leitor.nextDouble();
 
         //Calcular a media
-        double media = aluno.calcularMedia();
+        double media = aluno1.calcularMedia();
 
         //Exibir a média
-        System.out.printf("A média do aluno " + aluno.nome + " foi: %.2f", media);
+        System.out.printf("A média do aluno1 " + aluno1.nome + " foi: %.2f", media);
     }
 }
