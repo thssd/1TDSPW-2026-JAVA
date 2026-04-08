6370c9398fa26b074f648c923c00ac0c15af73e9
thssd <thiagoshimizusodre@gmail.com>
2026-04-08 16:58:05 -0300

    aula 04

--- Apostila04-Exercicios/src/view/Terminal.java
+++ Apostila04-Exercicios/src/view/Terminal.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 
 public class Terminal {
     static void main() {
-        Scanner leitor = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Aluno aluno1 = new Aluno();
 
         //Ler os dados do aluno1
         System.out.println("Digite seu nome");
-        aluno1.nome = leitor.nextLine();
+        aluno1.nome = scanner.nextLine();
 
         System.out.println("Digite a nota da cp1");
-        aluno1.cp1 = leitor.nextDouble();
+        aluno1.cp1 = scanner.nextDouble();
 
         System.out.println("Digite a nota da cp2");
-        aluno1.cp2 = leitor.nextDouble();
+        aluno1.cp2 = scanner.nextDouble();
 
         System.out.println("Digite a nota do primeiro Challenge");
-        aluno1.sprint1 = leitor.nextDouble();
+        aluno1.sprint1 = scanner.nextDouble();
 
         System.out.println("Digite a nota do segundo Challenge");
-        aluno1.sprint2 = leitor.nextDouble();
+        aluno1.sprint2 = scanner.nextDouble();
 
         System.out.println("Digite da Global Solution");
-        aluno1.gs = leitor.nextDouble();
+        aluno1.gs = scanner.nextDouble();
 
         //Calcular a media
         double media = aluno1.calcularMedia();
